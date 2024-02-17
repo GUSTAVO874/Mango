73e2a80cce114b1ff60b469ad7bdabea5f983455
GUSTAVO874 <gustavo8156@gmail.com>
2024-02-16 22:46:36 -0500

    Agregar archivos de proyecto.

---
+++ Mango.Services.CouponAPI/Models/Dto/ResponseDto.cs
@@ -0,0 +1,10 @@
+﻿namespace Mango.Services.CouponAPI.Models.Dto
+{
+    //para pasar un objeto común y personalizado por la api
+    public class ResponseDto
+    {
+        public object? Result { get; set; }
+        public bool IsSuccess { get; set; } = true;
+        public string Message { get; set; } = "";
+    }
+}
---
+++ Mango.Web/Service/BaseService.cs
@@ -0,0 +1,86 @@
+﻿using Mango.Web.Models;
+using Mango.Web.Service.IService;
+using Newtonsoft.Json;
+using System.Net;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+//using Mango.Web.Utility;
+using static Mango.Web.Utility.SD;
+//using System.Net.Http.Json;
+
+namespace Mango.Web.Service
+{
+    public class BaseService : IBaseService
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+        public BaseService(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<ResponseDto?> SendAsync(RequestDto requestDto)
+        {
+            try
+            {
+                HttpClient client = _httpClientFactory.CreateClient("MangoAPI");
+                HttpRequestMessage message = new();
+                message.Headers.Add("Accept", "application/json");
+                //token
+
+                message.RequestUri = new Uri(requestDto.Url);
+                //esta condición se cumple solo cuando Data trae el objeto solo necesario para
+                //post y put el coupon, caso contrario data es siempre null
+                if (requestDto.Data != null)
+                {
+                    message.Content = new StringContent(JsonConvert.SerializeObject(requestDto.Data), Encoding.UTF8, "application/json");
+                }
+
+                HttpResponseMessage? apiResponse = null;
+
+                switch (requestDto.ApiType)
+                {
+                    case ApiType.POST:
+                        message.Method = HttpMethod.Post;
+                        break;
+                    case ApiType.DELETE:
+                        message.Method = HttpMethod.Delete;
+                        break;
+                    case ApiType.PUT:
+                        message.Method = HttpMethod.Put;
+                        break;
+                    default:
+                        message.Method = HttpMethod.Get;
+                        break;
+                }
+
+                apiResponse = await client.SendAsync(message);
+
+                switch (apiResponse.StatusCode)
+                {
+                    case HttpStatusCode.NotFound:
+                        return new() { IsSuccess = false, Message = "Not Found" };
+                    case HttpStatusCode.Forbidden:
+                        return new() { IsSuccess = false, Message = "Access denied" };
+                    case HttpStatusCode.Unauthorized:
+                        return new() { IsSuccess = false, Message = "unauthorized" };
+                    case HttpStatusCode.InternalServerError:
+                        return new() { IsSuccess = false, Message = "Internal server error" };
+                    default:
+                        var apiContent = await apiResponse.Content.ReadAsStringAsync();
+                        var apiResponseDto = JsonConvert.DeserializeObject<ResponseDto>(apiContent);
+                        return apiResponseDto;
+                }
+
+            }
+            catch (Exception ex)
+            {
+                var dto = new ResponseDto
+                {
+                    Message = ex.Message.ToString(),
+                    IsSuccess = false
+                };
+                return dto;
+            }
+        }
+    }
+}
